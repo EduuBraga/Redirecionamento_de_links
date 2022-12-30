f9ca0d48216dda26ef8cdbfe3fbd26f9a5948a94
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-12-30 15:37:24 -0300

    style: Adicionei estilo na página de links

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const routes = require('./routes/linkRoutes.js');
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'))
 app.use("/", routes);
 
 app.set('view engine', 'ejs')
