238cbcd4d2c9cd71176b65b8ac315c8789988bfc
Eduardo Braga <chocolate.blindado123@gmail.com>
2023-01-02 12:08:48 -0300

    Feat: criei funções para fechar e abrir o modal

--- public/assets/scripts/index.js
+++ public/assets/scripts/index.js
@@ -1,4 +1,7 @@
-const buttonsDeleteLink = document.querySelectorAll('.delete_link')
+const buttonsDeleteLink = document.querySelectorAll('.delete_link');
+const buttonOpenModal = document.querySelector('.open_modal');
+const buttonCloseModal = document.querySelector('.img-close_modal');
+const modal = document.querySelector('.container-modal-add');
 
 const deleteLink = async event => {
   const parentElementClicked = event.target.parentElement;
@@ -13,8 +16,25 @@ const deleteLink = async event => {
     const objError = await res.json();
     alert(objError.message);
   }
-}
+};
+
+const handleOpenModal = () => {
+  modal.style = 'display: flex;';
+};
+
+const handleCloseModal = (event) => {
+  const tagElementClicked = event.target.tagName;
+  const tagsCloseModal = ['SECTION', 'IMG'];
+  const modalCanClose = tagsCloseModal.includes(tagElementClicked);
+
+  if(modalCanClose){
+    modal.style.display = 'none';
+  }
+};;
 
 buttonsDeleteLink.forEach(button => {
-  button.addEventListener('click', deleteLink)
+  button.addEventListener('click', deleteLink);
 });
+
+buttonOpenModal.addEventListener('click', handleOpenModal);
+modal.addEventListener('click', handleCloseModal);
